c96bade617a05615f0368593e4ffb2c9c1a835dc
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-11 16:23:04 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -1,32 +1,31 @@
 // variables 
 const game = document.getElementById("game")
 
-for(let x = 1; x <= 10; x++) {
-    for(let y = 1; y <= 7; y++){
-        // variables box
-        let bg;
-        let type;
+for (let y = 1; y <= 100; y++) {
+    // variables box
+    let bg;
+    let type;
 
-        // conditions
-        if(y % 3 === 0 && y % 5 === 0){
-            type = "fizz"
-            bg = "fizz-bg"
-        } else if(y % 5 === 0){
-            type = "buzz"
-            bg = "buzz-bg"
-        } else if(y % 3 === 0){
-            type = "fizzBuzz"
-            bg = "fizzBuzz-bg"
-        } else {
-            type = y
-            bg = "standard-bg"
-        }
-
-        // creation
-        const boxEl = document.createElement("div")
-        boxEl.className = `square ${bg} d-flex justify-content-center align-items-center`
-        boxEl.innerHTML = `${type}`
-        game.append(boxEl)
+    // conditions
+    if (y % 3 === 0 && y % 5 === 0) {
+        type = "Fizz"
+        bg = "fizz-bg"
+    } else if (y % 5 === 0) {
+        type = "Buzz"
+        bg = "buzz-bg"
+    } else if (y % 3 === 0) {
+        type = "FizzBuzz"
+        bg = "fizzBuzz-bg"
+    } else {
+        type = y
+        bg = "standard-bg"
     }
+
+    // creation
+    const boxEl = document.createElement("div")
+    boxEl.className = `square ${bg} d-flex justify-content-center align-items-center`
+    boxEl.innerHTML = `${type}`
+    game.append(boxEl)
 }
+
 
