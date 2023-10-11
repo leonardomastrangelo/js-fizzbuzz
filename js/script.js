3c0c7754b89e05281a68cc0b220c506b14a163be
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-11 15:34:45 +0200

    algorithm inserted but visibility problem

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
-let template = 
-`<div class="col-2 p-2">
-<div class="square d-flex justify-content-center align-items-center">prova</div>
-</div>
-`
+// variables 
 const game = document.getElementById("game")
-const divEl = document.createElement("div");
-divEl.innerHTML = template;
-game.append(divEl)
-console.log(template, game, divEl)
+
+for(let x = 1; x <= 10; x++) {
+    for(let y = 1; y <= 8; y++){
+        // variables box
+        let bg;
+        let type;
+
+        // conditions
+        if(y % 3 === 0){
+            type = "fizz"
+            bg = "fizz-bg"
+        } else if(y % 5 === 0){
+            type = "buzz"
+            bg = "buzz-bg"
+        } else if(y % 3 === 0 && y % 5 === 0){
+            type = "fizzBuzz"
+            bg = "fizzBuzz-bg"
+        } else {
+            type = y
+            bg = "standard-bg"
+        }
+
+        // creation
+        const boxEl = document.createElement("div")
+        boxEl.className = `square ${bg} d-flex justify-content-center align-items-center`
+        boxEl.innerHTML = `${type}`
+        console.log(boxEl)
+    }
+}
+
