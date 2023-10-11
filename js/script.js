d430225d0ad6c32ffee916dfef5ddee072f8257f
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-11 15:56:54 +0200

    correction on visibility

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@
 const game = document.getElementById("game")
 
 for(let x = 1; x <= 10; x++) {
-    for(let y = 1; y <= 8; y++){
+    for(let y = 1; y <= 7; y++){
         // variables box
         let bg;
         let type;
 
         // conditions
-        if(y % 3 === 0){
+        if(y % 3 === 0 && y % 5 === 0){
             type = "fizz"
             bg = "fizz-bg"
         } else if(y % 5 === 0){
             type = "buzz"
             bg = "buzz-bg"
-        } else if(y % 3 === 0 && y % 5 === 0){
+        } else if(y % 3 === 0){
             type = "fizzBuzz"
             bg = "fizzBuzz-bg"
         } else {
@@ -26,7 +26,7 @@ for(let x = 1; x <= 10; x++) {
         const boxEl = document.createElement("div")
         boxEl.className = `square ${bg} d-flex justify-content-center align-items-center`
         boxEl.innerHTML = `${type}`
-        console.log(boxEl)
+        game.append(boxEl)
     }
 }
 
